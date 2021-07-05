6d1f63d861de8de9161619310489585a96fdb6c6
Simon Júlia <simon.julia@nexogen.hu>
2021-07-05 11:15:10 +0200

    refactor: select indicator button functionality

--- src/components/IndexSelection/IndexSelection.tsx
+++ src/components/IndexSelection/IndexSelection.tsx
@@ -1,12 +1,55 @@
+import React, { useState } from 'react';
 import styles from './IndexSelection.module.scss';
 import SelectButton from './SelectButton/SelectButton';
 import { covidIndices  } from '../../constants/constants';
+import { useAppDispatch } from '../../hooks/reduxHooks';
+import { addSelectedIndex, removeSelectedIndex } from '../../store/dataSlice';
+
+const covidIndicesArray = Array.from(covidIndices);
+const initialButtonsState = covidIndicesArray.map(index => false);
 
 const IndexSelection: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [buttonsState, setButtonsState] = useState(initialButtonsState);
+    const dispatch = useAppDispatch();
+
+    const handleClick = (ev: React.MouseEvent) => {
+        const indicator = ev.currentTarget.getAttribute('data-id');
+
+        if (indicator === 'custom' && !buttonsState[covidIndicesArray.indexOf('custom')]){
+            setIsOpen(true);
+        }
+
+        if (!!indicator && covidIndices.has(indicator)){
+            const currentIndex = covidIndicesArray.indexOf(indicator);
+            if (buttonsState[currentIndex]){
+                dispatch(removeSelectedIndex(indicator));
+            } else {
+                dispatch(addSelectedIndex(indicator));
+            }
+            setButtonsState(prev => {
+                const newState = [...prev];
+                newState[currentIndex] = !newState[currentIndex];
+                return newState;
+            });
+        }
+
+    };
+
+    const handleClose = () => {
+        setIsOpen(false);
+    };
 
     return (
         <div className={styles.selectionArea}>
-            { Array.from(covidIndices).map(inx => <SelectButton key={inx} name={inx} />)}
+            { Array.from(covidIndices).map((indicator, inx) => <SelectButton 
+            key={indicator} 
+            name={indicator}
+            active={buttonsState[inx]}
+            onClick={handleClick}
+            onClose={handleClose}
+            isOpen={isOpen}
+             />)}
         </div>
     );
 };
--- src/components/IndexSelection/SelectButton/SelectButton.tsx
+++ src/components/IndexSelection/SelectButton/SelectButton.tsx
@@ -1,35 +1,43 @@
-import React, { useState } from 'react';
-import { useAppDispatch } from '../../../hooks/reduxHooks';
-import { addSelectedIndex, removeSelectedIndex } from '../../../store/dataSlice';
+import React from 'react';
+// import { useAppDispatch } from '../../../hooks/reduxHooks';
+// import { addSelectedIndex, removeSelectedIndex } from '../../../store/dataSlice';
 import styles from './SelectButton.module.scss';
 import CustomSelection from '../CustomSelection/CustomSelection';
 
-const SelectButton: React.FC<{name: string}> = ({ name }) => {
-    const [active, setActive] = useState(false);
-    const [isOpen, setIsOpen] = useState(false);
-    const dispatch = useAppDispatch();
+const SelectButton: React.FC<{
+    name: string, 
+    active: boolean, 
+    isOpen: boolean,
+    onClose: () => void,
+    onClick: (ev: React.MouseEvent<HTMLButtonElement>) => void
+}> = ({ name, active, isOpen, onClick, onClose }) => {
+    // const [active, setActive] = useState(false);
+    // const [isOpen, setIsOpen] = useState(false);
+    // const dispatch = useAppDispatch();
 
-    const handleClose = () => {
-        setIsOpen(false);
-    }
+    // const handleClose = () => {
+    //     setIsOpen(false);
+    // }
 
-    const handleClick = () => {
-        if (!active) {
-            if (name === 'custom'){
-                setIsOpen(true);
-            }
-            dispatch(addSelectedIndex(name));
-        } else dispatch(removeSelectedIndex(name));
-        setActive(prev => !prev);
-    };
+    // const handleClick = () => {
+    //     if (!active) {
+    //         if (name === 'custom'){
+    //             setIsOpen(true);
+    //         }
+    //         dispatch(addSelectedIndex(name));
+    //     } else dispatch(removeSelectedIndex(name));
+    //     setActive(prev => !prev);
+    // };
 
     return (
         <>
         <button className={active ? styles.activeSelectButton : styles.selectButton}
-            onClick={handleClick}>
+            onClick={onClick}
+            data-id={name}
+            >
             {name}
         </button>
-        {isOpen && <CustomSelection onClose={handleClose} />}
+        {isOpen && <CustomSelection onClose={onClose} />}
         </>
     );
 };
